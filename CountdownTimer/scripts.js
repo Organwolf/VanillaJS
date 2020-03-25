118b7457fc8a21868671926ff2996fcecc67f711
Organwolf <aron.polner@gmail.com>
2020-03-25 19:43:10 +0100

    Countdown in 1.. 2.. 3..

--- CountdownTimer/scripts.js
+++ CountdownTimer/scripts.js
@@ -1,9 +1,13 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
-// A function which purpose in life is to count down
+// A function, which purpose in life is to count down
 function timer(seconds){
+    // clear any existing timers
+    clearInterval(countdown);
+
     const now = Date.now();             // Static date function
     const then = now + seconds * 1000;  // millisecs
     displayTimeLeft(seconds);           // siplay the first second
@@ -20,7 +24,7 @@ function timer(seconds){
     }, 1000);
 }
 
-// A function which purpose in life is to display the minutes and seconds left
+// A function, which purpose in life is to display the minutes and seconds left
 function displayTimeLeft(seconds){
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = seconds % 60;
@@ -37,4 +41,26 @@ function displayEndTime(timestamp) {
     endTime.textContent = `Be back at ${hour}:${minutes < 10 ? '0' : ''}${minutes}`
 }
 
-//at 15:16
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+
+// get value
+// parseInt
+// multiply by 60
+// pass to timer
+
+buttons.forEach(button => button.addEventListener('click', startTimer))
+
+document.customForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const mins = this.minutes.value;
+    timer(mins * 60)
+    this.reset();
+})
+
+
+// TODOS -> wrap hours/days/weeks/etc
+// pause and start stop button
+// UI/UX look nad feel bro
